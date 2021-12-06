913ba4b03ae9d98871295b94e7d8e9539b179175
wchizewski <90284175+wchizewski@users.noreply.github.com>
2021-12-06 14:47:32 -0700

    monday

--- script.js
+++ script.js
@@ -14,36 +14,42 @@ let uuImg = document.getElementById("untitledunmastered")
 let damnImg = document.getElementById("damn");
 let mousex, mousey;
 let mouseisclicked;
-let odx = 0
-let ody = 847
-let odPx = 127
-let odPy = -1000
-let s80x = 113
-let s80y = 847
-let s80Px = 127
-let s80Py = -1000
-let gkmcx = 226
-let gkmcy = 847
-let gkmcPx = 127
-let gkmcPy = -1000
-let tpabx = 339
-let tpaby = 847
-let tpabPx = 127
-let tpabPy = -1000
-let uux = 452
-let uuy = 847
-let uuPx = 127
-let uuPy = -1000
-let damnx = 565
-let damny = 847
-let damnPx = 127
-let damnPy = -1000
+let odx = 0;
+let ody = 847;
+let odPx = 127;
+let odPy = -1000;
+let s80x = 113;
+let s80y = 847;
+let s80Px = 127;
+let s80Py = -1000;
+let gkmcx = 226;
+let gkmcy = 847;
+let gkmcPx = 127;
+let gkmcPy = -1000;
+let tpabx = 339;
+let tpaby = 847;
+let tpabPx = 127;
+let tpabPy = -1000;
+let uux = 452;
+let uuy = 847;
+let uuPx = 127;
+let uuPy = -1000;
+let damnx = 565;
+let damny = 847;
+let damnPx = 127;
+let damnPy = -1000;
 let followmouse = "none";
+let odRow = "none";
+let s80Row = "none";
+let gkmcRow = "none";
+let tpabRow = "none";
+let uuRow = "none";
+let damnRow = "none";
+
+
 
 requestAnimationFrame(loop);
 function loop() {
-
-
 
     // tier list
     ctx.fillStyle = "rgb(26, 26, 26)"
@@ -130,156 +136,204 @@ function loop() {
     if (followmouse === "od") {
         if (mousey < 127) {
             odPy = 7
+            odRow = "S"
         } else if (mousey < 247) {
             odPy = 127
+            odRow = "A"
         } else if (mousey < 367) {
             odPy = 247
+            odRow = "B"
         } else if (mousey < 487) {
             odPy = 367
+            odRow = "C"
         } else if (mousey < 607) {
             odPy = 487
+            odRow = "D"
         } else if (mousey < 727) {
             odPy = 607
+            odRow = "E"
         } else if (mousey < 847) {
             odPy = 727
+            odRow = "F"
+        } else {
+            odPy = -1000;
+            odRow = "none"
         }
-    } else {
-        odPy = -1000;
     }
+
+
 
     if (followmouse === "s80") {
         if (mousey < 127) {
             s80Py = 7
+            s80Row = "S"
         } else if (mousey < 247) {
             s80Py = 127
+            s80Row = "A"
         } else if (mousey < 367) {
             s80Py = 247
+            s80Row = "B"
         } else if (mousey < 487) {
             s80Py = 367
+            s80Row = "C"
         } else if (mousey < 607) {
             s80Py = 487
+            s80Row = "D"
         } else if (mousey < 727) {
             s80Py = 607
+            s80Row = "E"
         } else if (mousey < 847) {
             s80Py = 727
+            s80Row = "F"
+        } else {
+            s80Py = -1000;
+            s80Row = "none"
         }
-    } else {
-        s80Py = -1000;
     }
 
     if (followmouse === "gkmc") {
         if (mousey < 127) {
             gkmcPy = 7
+            gkmcRow = "S"
         } else if (mousey < 247) {
             gkmcPy = 127
+            gkmcRow = "A"
         } else if (mousey < 367) {
             gkmcPy = 247
+            gkmcRow = "B"
         } else if (mousey < 487) {
             gkmcPy = 367
+            gkmcRow = "C"
         } else if (mousey < 607) {
             gkmcPy = 487
+            gkmcRow = "D"
         } else if (mousey < 727) {
             gkmcPy = 607
+            gkmcRow = "E"
         } else if (mousey < 847) {
             gkmcPy = 727
+            gkmcRow = "F"
+        } else {
+            gkmcPy = -1000;
+            gkmcRow = "none"
         }
-    } else {
-        gkmcPy = -1000;
     }
 
     if (followmouse === "tpab") {
         if (mousey < 127) {
             tpabPy = 7
+            tpabRow = "S"
         } else if (mousey < 247) {
             tpabPy = 127
+            tpabRow = "A"
         } else if (mousey < 367) {
             tpabPy = 247
+            tpabRow = "B"
         } else if (mousey < 487) {
             tpabPy = 367
+            tpabRow = "C"
         } else if (mousey < 607) {
             tpabPy = 487
+            tpabRow = "D"
         } else if (mousey < 727) {
             tpabPy = 607
+            tpabRow = "E"
         } else if (mousey < 847) {
             tpabPy = 727
+            tpabRow = "F"
+        } else {
+            tpabPy = -1000;
+            tpabRow = "none"
         }
-    } else {
-        tpabPy = -1000;
     }
 
     if (followmouse === "uu") {
         if (mousey < 127) {
             uuPy = 7
+            uuRow = "S"
         } else if (mousey < 247) {
             uuPy = 127
+            uuRow = "A"
         } else if (mousey < 367) {
             uuPy = 247
+            uuRow = "B"
         } else if (mousey < 487) {
             uuPy = 367
+            uuRow = "C"
         } else if (mousey < 607) {
             uuPy = 487
+            uuRow = "D"
         } else if (mousey < 727) {
             uuPy = 607
+            uuRow = "E"
         } else if (mousey < 847) {
             uuPy = 727
+            uuRow = "F"
+        } else {
+            uuPy = -1000;
+            uuRow = "none"
         }
-    } else {
-        uuPy = -1000;
     }
 
     if (followmouse === "damn") {
         if (mousey < 127) {
             damnPy = 7
+            damnRow = "S"
         } else if (mousey < 247) {
             damnPy = 127
+            damnRow = "A"
         } else if (mousey < 367) {
             damnPy = 247
+            damnRow = "B"
         } else if (mousey < 487) {
             damnPy = 367
+            damnRow = "C"
         } else if (mousey < 607) {
             damnPy = 487
+            damnRow = "D"
         } else if (mousey < 727) {
             damnPy = 607
+            damnRow = "E"
         } else if (mousey < 847) {
             damnPy = 727
+            damnRow = "F"
+        } else {
+            damnPy = -1000;
+            damnRow = "none"
         }
-    } else {
-        damnPy = -1000;
     }
 
-    // if (followmouse === "s80") {
-    //     if (mousey < 127) {
-    //         s80Py = 7
-    //     }
-    // }
-    // if (followmouse === "gkmc") {
-    //     if (mousey < 127) {
-    //         gkmcPy = 7
-    //     }
-    // }
-    // if (followmouse === "tpab") {
-    //     if (mousey < 127) {
-    //         tpabPy = 7
-    //     }
-    // }
-    // if (followmouse === "uu") {
-    //     if (mousey < 127) {
-    //         uuPy = 7
-    //     }
-    // }
-    // if (followmouse === "damn") {
-    //     if (mousey < 127) {
-    //         damnPy = 7
-    //     }
-    // }
+// if (followmouse === "s80") {
+//     if (mousey < 127) {
+//         s80Py = 7
+//     }
+// }
+// if (followmouse === "gkmc") {
+//     if (mousey < 127) {
+//         gkmcPy = 7
+//     }
+// }
+// if (followmouse === "tpab") {
+//     if (mousey < 127) {
+//         tpabPy = 7
+//     }
+// }
+// if (followmouse === "uu") {
+//     if (mousey < 127) {
+//         uuPy = 7
+//     }
+// }
+// if (followmouse === "damn") {
+//     if (mousey < 127) {
+//         damnPy = 7
+//     }
+// }
 
-
-
-
-
-
-    requestAnimationFrame(loop);
+requestAnimationFrame(loop);
 }
+
+
+
 
 // event listrebnersd
 document.addEventListener("mousemove", mousemovemanager);
@@ -287,11 +341,10 @@ document.addEventListener("mousedown", mousedownmanager);
 document.addEventListener("mouseup", mouseupmanager);
 
 function mouseupmanager() {
-    mouseisclicked = false;
     if (followmouse === "od") {
         odx = odPx
         ody = odPy
-    } else if (followmouse === "s80" ) {
+    } else if (followmouse === "s80") {
         s80x = s80Px
         s80y = s80Py
     } else if (followmouse === "gkmc") {
@@ -303,14 +356,14 @@ function mouseupmanager() {
     } else if (followmouse === "uu") {
         uux = uuPx
         uuy = uuPy
-    } else {
+    } else if (followmouse === "damn") {
         damnx = damnPx
         damny = damnPy
     }
     followmouse = "none"
-    
-
+    mouseisclicked = false;
 }
+
 
 function mousedownmanager() {
     mouseisclicked = true;
@@ -341,5 +394,3 @@ function mousemovemanager(event) {
     mousex = event.x - cnvRect.x
     mousey = event.y - cnvRect.y
 }
-
-
